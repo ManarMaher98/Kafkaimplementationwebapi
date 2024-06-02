2ff7138b238a780cc057b00b4db4f0d0c5406efb
ManarMaher98 <manar-maher@spatiumsoftware.com>
2024-06-02 15:27:34 +0300

    Add project files.

---
+++ Controllers/ConsumerController.cs
@@ -0,0 +1,24 @@
+﻿using Kafkaproducerimplemenation.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KafkaImplementation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public InventoryController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult princesAdded([FromBody] Princesrequest request)
+        {
+            return Ok("princes added processed successfully.");
+        }
+    }
+}
